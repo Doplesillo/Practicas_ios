4ff79c93131c4e1ca68640e33b56551556a9f26b
Jose Manuel Chairez Macias <manuelchairezudg@gmail.com>
2018-05-21 13:34:05 -0500

    Initial Draft

---
+++ Food_Menu/Food_Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food_Menu
+//
+//  Created by Jose Manuel Chairez Macias on 21/05/18.
+//  Copyright © 2018 Jose Manuel Chairez Macias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
